6bad3ff7c84c5ce0260751fb460391d790e80d11
Koripriyan <koripriyan@gmail.com>
2024-01-20 09:15:37 +0530

    First commit

---
+++ src/test/java/com/Pages/WebElementLocators.java
@@ -0,0 +1,95 @@
+package com.Pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class WebElementLocators {
+
+	// Project 1 WebTesting
+
+	public static By DraggableDebitBank = By.xpath("//li[@class='block14 ui-draggable']/a[@class=\"button button-orange\"][1]");
+	public static By DroppableAccountBank = By.xpath("//ol[@class='field14 ui-droppable ui-sortable']");
+	public static By DraggableDebitAmount = By.xpath("//li[@class='block13 ui-draggable']/preceding-sibling::li[@id='fourth']");
+	public static By DroppableDebitAmount = By.xpath("//ol[@class='field13 ui-droppable ui-sortable' and @id='amt7']/li");
+	public static By DraggableCreditSales = By.xpath("//li[@class='block15 ui-draggable']/a[@class='button button-orange']");
+	public static By DroppableCreditSales = By.xpath("//ol[@class='field15 ui-droppable ui-sortable']");
+	public static By DroppableCreditAmount = By.xpath("//ol[@class='field13 ui-droppable ui-sortable' and @id='amt8']");
+	public static By DateOfBirthInput = By.id("dateOfBirthInput");
+	public static By ElementMonth = By.xpath("//select[@class=\\\"react-datepicker__month-select\\\"]");
+	public static By ElementYear = By.xpath("//select[@class=\\\"react-datepicker__year-select\\\"]");
+	public static By ElementDay = By.xpath("//div[@class=\\\"react-datepicker__week\\\"]/div[text()='5']");
+	public static By SeleniumLink = By.xpath("//a[@class='dropdown-toggle' and contains(text(),'Selenium')]");
+	public static By RadioAndCheckBoxLink = By.xpath("//a[contains(text(),'Radio & Checkbox Demo')]");	
+	public static By RadioOption1 = By.xpath("//input[@type='radio' and @value='Option 1']");
+	public static By RadioOption2 = By.xpath("//input[@type='radio' and @value='Option 2']");
+	public static By RadioOption3 = By.xpath("//input[@type='radio' and @value='Option 3']");	
+	public static By CheckBoxOption1 = By.xpath("//input[@type='checkbox' and @value='checkbox1']");
+	public static By CheckBoxOption2 = By.xpath("//input[@type='checkbox' and @value='checkbox2']");
+	public static By CheckBoxOption3 = By.xpath("//input[@type='checkbox' and @value='checkbox3']");	
+	public static By SliderElement = By.xpath("//div[contains(text(),'Resizable box, starting at 200x200')]/following-sibling::span");
+	public static By Slider = By.xpath("//input[@type='range']");
+	public static By ResizeElement = By.xpath("//div[contains(text(),'Resizable box, starting at 200x200')]/following-sibling::span");
+	public static By SwitchToLink = By.xpath("//li/a[contains(text(),'SwitchTo')]");
+	public static By AlertsLink = By.xpath("//ul[@class='dropdown-menu']/li/a[contains(text(),'Alerts')]");
+	public static By Frameslnk = By.xpath("//ul[@class='dropdown-menu']/li/a[contains(text(),'Frames')]");
+	public static By AlertWithOk = By.xpath("//li/a[@href='#OKTab' and contains(text(),'Alert with OK ')]");
+	public static By AlertWithOkAndCancel = By.xpath("//li/a[@href=\"#CancelTab\" and contains(text(),'Alert with OK & Cancel')]");
+	public static By AlertWithTextBox = By.xpath("//li/a[@href='#Textbox' and contains(text(),'Alert with Textbox')]");
+	public static By AlertsButtonlnk =  By.xpath("//button[contains(text(),'click the button to display an  alert box:')]");
+	public static By AlertsWithConfirmButtonlnk = By.xpath("//button[contains(text(),'click the button to display a confirm box')]");
+	public static By AlertsWithTextBoxlnk = By.xpath("//button[contains(text(),'click the button to display a confirm box')]");
+	public static By AlertsWithPromptlnk = By.xpath("//button[contains(text(),'click the button to demonstrate the prompt box')]");
+	public static By SingleIframelnk = By.xpath("//a[contains(text(),'Single Iframe')]"); 
+	public static By IframeIframelnk = By.xpath("//a[contains(text(),'Iframe with in an Iframe')]"); 
+	public static By TextBoxInsideFrame = By.xpath("//input[@type='text']"); 
+	public static By MultiFrame = By.xpath("//iframe[@src='MultipleFrames.html']"); 
+	public static By SingleFrame = By.xpath("//iframe[@style='float: left;height: 250px;width: 400px']");
+	public static By DatePicker = By.xpath("//input[@id='datepicker1']");
+	public static By DateSel = By.xpath("//table[@class='ui-datepicker-calendar']/tbody/tr/td/a[text()=5]");
+	public static By MonthYearSel = By.xpath("//a[@title='Next']");
+	public static By Morelnk = By.xpath("//a[@class='dropdown-toggle' and contains(text(),'More')]");
+	public static By Modallnk = By.xpath("//li/a[contains(text(),'Modals')]");
+	public static By ModalButtonlnk = By.xpath("//a[@data-toggle='modal' and @href='#myModal']");
+	public static By ModalButtonlnk2 = By.xpath("//a[@data-toggle='modal' and @href='#myModal2']");
+	public static By MultiModalButtonlnk = By.xpath("//a[@data-toggle='modal' and @href='#myModalMulti']");
+	public static By ModalWindowTitle = By.xpath("//h4[@class='modal-title' and contains(text(),'Modal title')]");
+	public static By ModalCloseBtn = By.xpath("//div[@class='modal-footer']/button[@data-dismiss='modal' and contains(text(),'Close')]");
+	public static By CloselnkModal2 = By.xpath("//a[contains(text(),'Close')]");
+	
+	public static By Registerlink = By.xpath("//a[contains(text(),'REGISTER')]");
+	public static By firstName = By.xpath("//input[@name='firstName']");
+	public static By lastName = By.xpath("//input[@name='lastName']");
+	public static By phone = By.xpath("//input[@name='phone']");
+	public static By userName = By.xpath("//input[@name='userName']");
+	public static By address1 = By.xpath("//input[@name='address1']");
+	public static By city = By.xpath("//input[@name='city']");
+	public static By state = By.xpath("//input[@name='state']");
+	public static By postalCode = By.xpath("//input[@name='postalCode']");
+	public static By country = By.xpath("//select[@name='country']");
+	public static By email = By.xpath("//input[@name='email']");
+	public static By password = By.xpath("//input[@name='password']");
+	public static By confirmPassword = By.xpath("//input[@name='confirmPassword']");
+	public static By SignOn = By.xpath("//a[contains(text(),'SIGN-ON')]");
+	public static By SignIn = By.xpath("//a[contains(text(),' sign-in ')]");
+	public static By LoginuserName = By.xpath("//input[@name='userName']");
+	public static By LoginPassword = By.xpath("//input[@name='password']");
+	public static By Submit = By.xpath("//input[@type=\"submit\"]");
+
+	public static void SelectValueFromDropdown(List<WebElement> AllElements, WebElement DropDownValue) {
+		// TODO Auto-generated method stub
+		int TotalElements = AllElements.size();
+
+		for (int i = 1; i <= TotalElements - 1; i++) {
+			String DropDownFromScript = AllElements.get(i).getText();
+
+			if (DropDownFromScript.equals(DropDownValue)) {
+				AllElements.get(i).click();
+				break;
+			}
+		}
+
+	}
+
+}
---
+++ src/test/java/com/WebSiteTesting/WebTesting.java
@@ -0,0 +1,324 @@
+package com.WebSiteTesting;
+
+import org.testng.annotations.Test;
+
+import com.Utility.Library;
+
+import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeSuite;
+import com.Pages.WebElementLocators;
+
+public class WebTesting extends Library {
+
+//	Test the login screen with username and password
+//	● Test online widgets like date pickers, tabs, sliders
+//	● Test interactive actions like draggable, resizable, selectable etc.
+//	● Test the filling up of registration form and submission
+//	● Test frames and windows
+//	● Test drop-down menus, alert boxes
+
+	@BeforeSuite
+	public void beforeSuite() throws IOException {
+		ReadPropertiesFile();
+		System.setProperty("webdriver.gecko.driver",objProperties.getProperty("FireFoxDriverPath"));
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+	}
+
+	@Test(priority = 1, enabled = false)
+	public void RegisterScreenTest() throws InterruptedException {
+
+		driver.navigate().to(objProperties.getProperty("RegisterScreenLink"));
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+		driver.findElement(WebElementLocators.Registerlink).click();
+		driver.findElement(WebElementLocators.firstName).sendKeys(objProperties.getProperty("firstname"));
+		driver.findElement(WebElementLocators.lastName).sendKeys(objProperties.getProperty("lastName"));
+		driver.findElement(WebElementLocators.phone).sendKeys(objProperties.getProperty("phone"));
+		driver.findElement(WebElementLocators.userName).sendKeys(objProperties.getProperty("userName"));
+		driver.findElement(WebElementLocators.address1).sendKeys(objProperties.getProperty("address1"));
+		driver.findElement(WebElementLocators.city).sendKeys(objProperties.getProperty("city"));
+		driver.findElement(WebElementLocators.state).sendKeys(objProperties.getProperty("state"));
+		driver.findElement(WebElementLocators.postalCode).sendKeys(objProperties.getProperty("postalCode"));
+		WebElement Country = driver.findElement(WebElementLocators.country);
+		Select SelCountry = new Select(Country);
+		SelCountry.selectByValue(objProperties.getProperty("country"));
+		Thread.sleep(2000);
+		driver.findElement(WebElementLocators.email).sendKeys(objProperties.getProperty("email"));
+		driver.findElement(WebElementLocators.password).sendKeys(objProperties.getProperty("password"));
+		driver.findElement(WebElementLocators.confirmPassword).sendKeys(objProperties.getProperty("confirmPassword"));
+		driver.findElement(WebElementLocators.Submit).click();
+		Thread.sleep(2000);
+		driver.findElement(WebElementLocators.SignIn).click();
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 2, enabled = false)
+	public void LoginTest() throws InterruptedException {
+		WebDriverWait wbContinue = new WebDriverWait(driver, 50);
+		WebElement ContinueElem = wbContinue
+				.until(ExpectedConditions.visibilityOfElementLocated(WebElementLocators.LoginuserName));
+		driver.findElement(WebElementLocators.LoginuserName).sendKeys(objProperties.getProperty("loginUsername"));
+		driver.findElement(WebElementLocators.LoginPassword).sendKeys(objProperties.getProperty("loginPassword"));
+		driver.findElement(WebElementLocators.Submit).click();
+	}
+
+	@Test(priority = 3, enabled = false)
+	public void DragAndDrop() throws InterruptedException {
+
+		driver.navigate().to(objProperties.getProperty("DragAndDropLink"));
+		Thread.sleep(5000);
+
+		WebElement DraggableDebitBank = driver.findElement(WebElementLocators.DraggableDebitBank);
+		WebElement DroppableAccountBank = driver.findElement(WebElementLocators.DroppableAccountBank);
+		WebElement DraggableDebitAmount = driver.findElement(WebElementLocators.DraggableDebitAmount);
+		WebElement DroppableDebitAmount = driver.findElement(WebElementLocators.DroppableDebitAmount);
+		WebElement DraggableCreditSales = driver.findElement(WebElementLocators.DraggableCreditSales);
+		WebElement DroppableCreditSales = driver.findElement(WebElementLocators.DroppableCreditSales);
+		WebElement DroppableCreditAmount = driver.findElement(WebElementLocators.DroppableCreditAmount);
+
+		Actions objActions = new Actions(driver);
+
+		objActions.dragAndDrop(DraggableDebitBank, DroppableAccountBank).build().perform();
+		objActions.dragAndDrop(DraggableDebitAmount, DroppableDebitAmount).build().perform();
+
+		objActions.dragAndDrop(DraggableCreditSales, DroppableCreditSales).build().perform();
+		objActions.dragAndDrop(DraggableDebitAmount, DroppableCreditAmount).build().perform();
+
+	}
+
+	@Test(priority = 4, enabled = false)
+	public void DatePicker() throws InterruptedException {
+		try {
+			driver.navigate().to(objProperties.getProperty("DatePickerLink"));
+			WebDriverWait wbContinue = new WebDriverWait(driver, 50);
+			WebElement ContinueElem = wbContinue.until(ExpectedConditions.visibilityOfElementLocated(WebElementLocators.DatePicker));
+			driver.findElement(WebElementLocators.DatePicker).click();		
+			driver.findElement(WebElementLocators.DateSel).click();
+		}
+
+		catch (StaleElementReferenceException e) {
+			WebElement SelMonthYear = driver.findElement(WebElementLocators.MonthYearSel);
+
+			Actions objAction = new Actions(driver);			
+			int clicks = 5;
+
+			for (int i = 0; i < clicks; i++) {
+				objAction.click(SelMonthYear).build().perform();
+			}
+			
+			
+
+		}
+	}
+	
+	@Test(priority = 5,enabled=false)
+	public void RadioAndCheckBoxValidate() throws InterruptedException {
+		
+		driver.navigate().to(objProperties.getProperty("NewToursLink"));
+        driver.findElement(WebElementLocators.SeleniumLink).click();
+        Thread.sleep(5000);      
+//        WebDriverWait wbContinue = new WebDriverWait(driver, 50);
+//		WebElement ContinueElem = wbContinue
+//				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Radio & Checkbox Demo')]")));
+        driver.findElement(WebElementLocators.RadioAndCheckBoxLink).click();
+        
+        WebDriverWait wbContinue2 = new WebDriverWait(driver, 50);
+		WebElement ContinueElem2 = wbContinue2.until(ExpectedConditions.visibilityOfElementLocated(WebElementLocators.RadioOption2));
+        
+		Thread.sleep(5000);
+		
+		driver.findElement(WebElementLocators.RadioOption2).click();
+        driver.findElement(WebElementLocators.CheckBoxOption3).click();
+        
+        WebElement OptionValue =   driver.findElement(WebElementLocators.RadioOption2);
+        WebElement CheckboxValue =    driver.findElement(WebElementLocators.CheckBoxOption3);
+        String OptVal =  OptionValue.getAttribute("value");
+        String CheckBoxVal =  CheckboxValue.getAttribute("value");
+        
+        System.out.println("OptionValue-"+OptVal);
+        System.out.println("CheckboxValue-"+CheckBoxVal);
+        
+        WebElement Radiobutton1 = driver.findElement(WebElementLocators.RadioOption1);
+        WebElement Radiobutton2 = driver.findElement(WebElementLocators.RadioOption2);
+        WebElement Radiobutton3 = driver.findElement(WebElementLocators.RadioOption3);
+        
+        WebElement CheckBox1 = driver.findElement(WebElementLocators.CheckBoxOption1);
+        WebElement CheckBox2 = driver.findElement(WebElementLocators.CheckBoxOption2);
+        WebElement CheckBox3 = driver.findElement(WebElementLocators.CheckBoxOption3);
+        
+        if(Radiobutton1.isSelected()) {System.out.println("Option 1 Selected");}      
+        if(Radiobutton2.isSelected()){System.out.println("Option 2 Selected");}    
+        if(Radiobutton3.isSelected()){System.out.println("Option 3 Selected");}     
+        if(CheckBox1.isSelected()){System.out.println("checkbox 1 Selected");}      
+        if(CheckBox2.isSelected()){System.out.println("checkbox 2 Selected");}       
+        if(CheckBox3.isSelected()){System.out.println("checkbox 3 Selected");}
+        
+		
+	}
+	
+	@Test(priority = 6,enabled=false)
+	public void Slider()  {
+		driver.navigate().to(objProperties.getProperty("SliderLink"));	
+		WebElement slider = driver.findElement(WebElementLocators.Slider); // Replace "slider-id" with the actual ID of your slider element
+		int sliderWidth = 80;
+		Actions actions = new Actions(driver);
+		actions.clickAndHold(slider).moveByOffset(sliderWidth, 0).release().perform();
+
+	}
+	
+	@Test(priority = 7,enabled=false)
+	public void Resizable()  {
+		driver.navigate().to(objProperties.getProperty("ResizeableLink"));
+		
+		WebElement divElement = driver.findElement(WebElementLocators.ResizeElement); // Replace "slider-id" with the actual ID of your slider element
+		resizeDivWithActions(driver, divElement, 50, 30);
+	}
+	
+	// Alert Popup, Prompt Window
+		@Test(priority = 8, enabled = false)
+		public void AlertWindow() throws InterruptedException {
+			driver.navigate().to(objProperties.getProperty("AutomationLink"));
+			driver.findElement(WebElementLocators.SwitchToLink).click();
+//			Thread.sleep(5000);
+			WebDriverWait wbContinue = new WebDriverWait(driver, 50);
+			WebElement ContinueElem = wbContinue
+					.until(ExpectedConditions.visibilityOfElementLocated(WebElementLocators.AlertsLink));
+			driver.findElement(WebElementLocators.AlertsLink).click();
+			WebDriverWait wbContinue2 = new WebDriverWait(driver, 50);
+			WebElement ContinueElem2 = wbContinue
+					.until(ExpectedConditions.visibilityOfElementLocated(WebElementLocators.AlertsButtonlnk));
+			driver.findElement(WebElementLocators.AlertsButtonlnk).click();
+			Alert alert = driver.switchTo().alert();
+			Thread.sleep(3000);
+			alert.accept();
+			driver.findElement(WebElementLocators.AlertWithOkAndCancel).click();
+			driver.findElement(WebElementLocators.AlertsWithConfirmButtonlnk).click();
+			alert.accept();
+			driver.findElement(WebElementLocators.AlertWithTextBox).click();
+			driver.findElement(WebElementLocators.AlertsWithPromptlnk).click();
+			alert.sendKeys("");
+			Thread.sleep(5000);
+			alert.sendKeys("This is test");
+			alert.accept();
+			driver.findElement(WebElementLocators.AlertsWithPromptlnk).click();
+			Thread.sleep(5000);
+			alert.dismiss();
+		}
+
+		// Switching to Frame
+		@Test(priority = 9, enabled = false)
+		public void SwitchingToFrame() throws InterruptedException {
+
+			driver.navigate().to(objProperties.getProperty("AutomationLink"));
+			driver.findElement(WebElementLocators.SwitchToLink).click();
+			driver.findElement(WebElementLocators.Frameslnk).click();
+			WebDriverWait wbContinue = new WebDriverWait(driver, 50);
+			WebElement ContinueElem = wbContinue
+					.until(ExpectedConditions.visibilityOfElementLocated(WebElementLocators.SingleIframelnk));
+			driver.findElement(WebElementLocators.IframeIframelnk).click();
+			WebElement Multiframeelem = driver.findElement(WebElementLocators.MultiFrame);
+
+			driver.switchTo().frame(Multiframeelem);
+			WebElement Singleeelem = driver.findElement(WebElementLocators.SingleFrame);
+			driver.switchTo().frame(Singleeelem);
+			Thread.sleep(2000);
+			driver.findElement(WebElementLocators.TextBoxInsideFrame).sendKeys("Test");
+			driver.switchTo().defaultContent();
+		}
+		
+		// Switching to Single Modal Window
+				@Test(priority = 9, enabled = true)
+				public void SwitchingToModal() throws InterruptedException {
+
+					driver.navigate().to(objProperties.getProperty("AutomationLink"));
+					driver.findElement(WebElementLocators.Morelnk).click();
+					driver.findElement(WebElementLocators.Modallnk).click();
+					WebDriverWait wbContinue = new WebDriverWait(driver, 50);
+					WebElement ContinueElem = wbContinue
+							.until(ExpectedConditions.visibilityOfElementLocated(WebElementLocators.ModalButtonlnk));
+					driver.findElement(WebElementLocators.ModalButtonlnk).click();
+					//Get current window
+					String mainWindowHandle = driver.getWindowHandle();
+					
+					//Get All window handles
+					Set<String> allWindowHandles = driver.getWindowHandles();
+					
+					for(String Handle:allWindowHandles)
+					{
+						    //Handle.hashCode();
+							driver.switchTo().window(Handle);
+							//String Title = driver.findElement(WebElementLocators.ModalWindowTitle).getText();
+							String Title = driver.findElement(By.xpath("//h4[@class='modal-title' and contains(text(),'Modal title')]")).getText();	
+							System.out.println("Title-" +Title);
+							//driver.findElement(WebElementLocators.ModalCloseBtn).click();
+							
+					}
+					
+				}
+				
+				// Switching to Multiple Modal Window
+				
+				@Test(priority = 10, enabled = true)
+				public void SwitchingToMultipleModal() throws InterruptedException {
+
+					driver.navigate().to(objProperties.getProperty("AutomationLink"));
+					driver.findElement(WebElementLocators.Morelnk).click();
+					driver.findElement(WebElementLocators.Modallnk).click();
+					WebDriverWait wbContinue = new WebDriverWait(driver, 50);
+					WebElement ContinueElem = wbContinue
+							.until(ExpectedConditions.visibilityOfElementLocated(WebElementLocators.MultiModalButtonlnk));
+					driver.findElement(WebElementLocators.MultiModalButtonlnk).click();
+					//Get current window
+					String mainWindowHandle = driver.getWindowHandle();
+					
+					//Get All window handles
+					Set<String> allWindowHandles = driver.getWindowHandles();
+					
+					for(String Handle:allWindowHandles)
+					{
+						    //Handle.hashCode();
+							driver.switchTo().window(Handle);
+							//String Title = driver.findElement(WebElementLocators.ModalWindowTitle).getText();
+							String Title1 = driver.findElement(By.xpath("//h4[@class='modal-title' and contains(text(),'First Modal')]")).getText();	
+							System.out.println(Title1);
+							driver.findElement(WebElementLocators.ModalButtonlnk2).click();
+							driver.switchTo().window(Handle);
+							driver.findElement(WebElementLocators.CloselnkModal2).click();
+							Thread.sleep(5000);
+							//driver.switchTo().defaultContent();
+							//driver.findElement(WebElementLocators.CloselnkModal2).click();
+							driver.switchTo().defaultContent();
+					}
+					
+				}			
+			
+	
+	private static void resizeDivWithActions(WebDriver driver, WebElement element, int xOffset, int yOffset) {
+        Actions builder = new Actions(driver);
+        builder.clickAndHold(element).moveByOffset(xOffset, yOffset).release().perform();
+        System.out.println("resize");
+    }
+
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Beginning Test");
+	}
+
+}
